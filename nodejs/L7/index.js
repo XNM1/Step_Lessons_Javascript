c0fb4be3a0e93c1a05c71add725fa54082869e1f
Артем Шевченко <artemx8zx@gmail.com>
2021-07-22 08:27:04 +0300

    L7 promise example update

--- nodejs/L7/index.js
+++ nodejs/L7/index.js
@@ -1,37 +1,35 @@
-function check(IAsyncResult, callback) {
-    let fc = () => {
-        if(IAsyncResult.length == 3) {
-            clearTimeout(timer);
-            callback(IAsyncResult);
-            return;
-        }
-        timer = setTimeout(fc);
-    }
-    let timer = setTimeout(fc);
-}
+// function check(IAsyncResult, callback) {
+//     let fc = () => {
+//         if(IAsyncResult.length == 3) {
+//             clearTimeout(timer);
+//             callback(IAsyncResult);
+//             return;
+//         }
+//         timer = setTimeout(fc);
+//     }
+//     let timer = setTimeout(fc);
+// }
 
-function fact(n, IAsyncResult) {
-    let r = 1;
-    let fc = () => {
-        r *= n;
-        n--;
-        if(n == 1) {
-            clearTimeout(timer);
-            IAsyncResult.push(r);
-            return;
-        }
-        timer = setTimeout(fc);
-    }
-    let timer = setTimeout(fc);
-}
+// function fact(n, IAsyncResult) {
+//     let r = 1;
+//     let fc = () => {
+//         r *= n;
+//         n--;
+//         if(n == 1) {
+//             clearTimeout(timer);
+//             IAsyncResult.push(r);
+//             return;
+//         }
+//         timer = setTimeout(fc);
+//     }
+//     let timer = setTimeout(fc);
+// }
 
-let ir = [];
-fact(160, ir);
-fact(140, ir);
-fact(20, ir);
-check(ir, r => console.log(r));
-//while(!ir.isDone) {}
-//console.log(ir.result);
+// let ir = [];
+// fact(160, ir);
+// fact(140, ir);
+// fact(20, ir);
+// check(ir, r => console.log(r));
 
 // function fact(n, callback) {
 //     let r = 1;
@@ -51,3 +49,27 @@ check(ir, r => console.log(r));
 // fact(150, r => console.log(`fact 150: ${r}`));
 // fact(170, r => console.log(`fact 170: ${r}`));
 // fact(14, r => console.log(`fact 14: ${r}`));
+
+
+function fact(n, callback) {
+    let r = 1;
+    let fc = () => {
+        r *= n;
+        n--;
+        if (n == 1) {
+            clearTimeout(timer);
+            callback(r);
+            return;
+        }
+        timer = setTimeout(fc);
+    }
+    let timer = setTimeout(fc);
+}
+
+function factAsync(n) {
+    return new Promise((resolve, reject) => fact(n, resolve));
+}
+
+factAsync(150).then(r => console.log(`fact 150: ${r}`));
+factAsync(170).then(r => console.log(`fact 170: ${r}`));
+factAsync(14).then(r => console.log(`fact 14: ${r}`));
